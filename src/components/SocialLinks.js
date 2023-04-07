347ce879b53f3c36ab0ab0d4e7e7d815aa192265
UsaidAsif12 <usaida863@gmail.com>
2023-04-07 14:41:09 +0500

    Social Links Fixed in Mobile

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -52,7 +52,7 @@ const SocialLinks = () => {
         
     ]
   return (
-    <div  className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
+    <div  className=' lg:flex flex-col top-[35%] left-0 fixed'>
         <ul>
             {links.map(({id,child,href,style})=>(
                  // eslint-disable-next-line no-useless-concat
